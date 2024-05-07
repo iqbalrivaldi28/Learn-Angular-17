f26efc094a7e1b4e80bbb50181eb6ec7cdd43f09
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-07 15:31:28 +0700

    feat: add displa-data api

--- src/app/components/display-json/display-json.component.ts
+++ src/app/components/display-json/display-json.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
-import jsonData from './data/users.json'
+import jsonData from './data/users.json';
+import { DataService } from './data/data.service';
 
+export interface dataApi {
+  id: number;
+  name: string;
+  email: string;
+}
 @Component({
   selector: 'app-display-json',
   standalone: true,
@@ -10,4 +16,21 @@ import jsonData from './data/users.json'
 })
 export class DisplayJsonComponent {
   users: any[] = jsonData.users;
+  apiUserData: dataApi[] = [];
+  error!: string;
+  complated: boolean = false;
+
+  title = 'Materi Display data From Json Local and API'
+
+  constructor(private dataService: DataService) {
+    this.getData()
+  }
+
+  getData() {
+    this.dataService.getUsers().subscribe({
+      next: (data: any) => (this.apiUserData = data),
+      error: (err) => (this.error = err),
+      complete: () => (this.complated = true),
+    });
+  }
 }
