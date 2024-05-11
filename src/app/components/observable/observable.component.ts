e057d56c0ae0ada5e2f4453e0781767b629fdd46
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-11 23:49:06 +0700

    feat: add observable with fetch data API

--- src/app/components/observable/observable.component.ts
+++ src/app/components/observable/observable.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Observable, Subscription, map } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -12,6 +13,10 @@ export class ObservableComponent {
   title: string = 'Materi Observable: ';
 
   subscription?: Subscription;
+  BASE_URL: string = 'https://jsonplaceholder.typicode.com/users';
+  data: any;
+
+  httpClient = inject(HttpClient);
 
   ngOnInit(): void {
     console.log('Observable Component');
@@ -22,13 +27,30 @@ export class ObservableComponent {
     });
 
     this.subscription = myObservale.subscribe({
-      next: (data) => console.log(data),
+      next: (data) => {
+        console.log(data);
+        this.data = data;
+      },
       error: (err) => console.log('Something wrong occured: ' + err),
       complete: () => console.log('done'),
+    });
+
+    //? From API
+    this.fetchData().subscribe({
+      next: (data) => console.log('Response:', data),
+      error: (err) => console.log('Error: ', err),
+      complete: () => console.log('Done'),
     });
   }
 
   ngOnDestroy(): void {
-    this.subscription?.unsubscribe()
+    this.subscription?.unsubscribe();
+  }
+
+  //? From API
+  fetchData() {
+    return this.httpClient
+      .get(this.BASE_URL)
+      // .pipe(map((response: any) => response.data));
   }
 }
