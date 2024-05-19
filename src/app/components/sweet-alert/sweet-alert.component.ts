983a4fdc66f7893a8f4da103ff5e30a3b335ab2f
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-20 02:21:49 +0700

    feat: add sweet aler

---
+++ src/app/components/sweet-alert/sweet-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-sweet-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './sweet-alert.component.html',
+  styleUrl: './sweet-alert.component.scss'
+})
+export class SweetAlertComponent {
+  title: string = 'Materi Sweet ALert: ';
+
+  sweetAlert(){
+    Swal.fire('Berhasil', 'Ini adalah Sweet Alert', 'success')
+  }
+}
